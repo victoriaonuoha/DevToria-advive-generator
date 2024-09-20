9b32e5cce7b5d2f01cb6f330da20ae79c18725ed
VICTORIA <onuohavictoria27august@gmail.com>
2024-09-20 15:01:08 +0100

    added loaded to the screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,16 @@
 console.log('heyyy babay')
 
+// Show loader
+function showLoader() {
+  document.getElementById('loader').classList.remove('hidden');
+  document.getElementById('content').classList.add('hidden');
+}
+
+// Hide loader
+function hideLoader() {
+  document.getElementById('loader').classList.add('hidden');
+  document.getElementById('content').classList.remove('hidden');
+}
 
 
 
@@ -10,6 +21,7 @@ console.log('heyyy babay')
 // for the main deal
 document.getElementById("adBtn").addEventListener('click', () => {
    const apiUrl = "https://api.adviceslip.com/advice";
+   showLoader();
     fetch(apiUrl)
     .then(response => {
         if(!response.ok){
@@ -21,11 +33,13 @@ document.getElementById("adBtn").addEventListener('click', () => {
     .then(data => {
         document.getElementById('advice').innerText = data.slip.advice;
         document.getElementById('iid').innerText= data.slip.id;
+        hideLoader();
 
     } )
     .catch(error => {
         console.error("Error fetching data:",error);
         document.getElementById('advice').innerText = 'Network unstable... Error fetching data';
+        hideLoader();
     } )
 
 });
